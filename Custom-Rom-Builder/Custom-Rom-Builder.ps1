bc00b8125f486e59f3245c6c139dead6ef6a6414
DericHuynh <HuynhDeric@gmail.com>
2023-09-17 23:03:47 -0600

    Added some argument and variable checking.
    Added some argument and variable checking
    Added some formatting.

--- Custom-Rom-Builder/Custom-Rom-Builder.ps1
+++ Custom-Rom-Builder/Custom-Rom-Builder.ps1
@@ -1,21 +1,31 @@
 param ($originalRomFileName);
 $Host.ui.RawUI.WindowTitle = "Name";
 
+Write-Host;
 if([string]::IsNullOrEmpty($originalRomFileName)) {
     Write-Host "No argument was provided for the original rom file name."
+    Write-Host;
     $originalRomFileName = Read-Host "Please enter the original rom file name inside the Input folder"
 } else {
     Write-Host Original Rom File name argument provided: $originalRomFileName
 }
 
-Write-Host;
 $pathTest = Test-Path -Path Input\$originalRomFileName -PathType Leaf;
+$fileExtensionTest = [IO.Path]::GetExtension($originalRomFileName);
 
-while((-Not $pathTest) -and (-Not [string]::IsNullOrEmpty($originalRomFileName))) {
-    Write-Host $originalRomFileName does not link to a valid original rom file inside the Input folder.;
+while((-Not $pathTest) -or (-Not ($fileExtensionTest -eq '.img')) -or ([string]::IsNullOrEmpty($originalRomFileName))) {
+    if([string]::IsNullOrEmpty($originalRomFileName)) {
+        Write-Host File name is entereed was null or empty.;
+    } elseif(-Not $pathTest ) {
+        Write-Host $originalRomFileName is not a valid path.;
+    } elseif(-Not ($fileExtensionTest -eq '.img')) {
+        Write-Host $originalRomFileName is not an image file.;
+    } 
+    Write-Host;
     $originalRomFileName = Read-Host Please enter the original rom file name inside the Input folder;
     $pathTest = Test-Path -Path Input\$originalRomFileName -PathType Leaf;
-    Write-Host;
+    $fileExtensionTest = [IO.Path]::GetExtension($originalRomFileName);
+    
 }
 
 Write-Output $originalRomFileName;
