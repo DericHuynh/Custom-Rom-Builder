b31edc9268e7ac1a3c70a9d4d2417293b64d528e
DericHuynh <HuynhDeric@gmail.com>
2023-09-18 20:01:45 -0600

    Argument checking
    Argument checking

--- Custom-Rom-Builder/Custom-Rom-Builder.ps1
+++ Custom-Rom-Builder/Custom-Rom-Builder.ps1
@@ -1,32 +1,71 @@
-param ($originalRomFileName);
-$Host.ui.RawUI.WindowTitle = "Name";
+param ($originalRomFilePath, $GSIFilePath);
 
-Write-Host;
+#Initial check to see if original rom path argument was provided or not.
+Write-Host ;
 if([string]::IsNullOrEmpty($originalRomFileName)) {
-    Write-Host "No argument was provided for the original rom file name."
-    Write-Host;
-    $originalRomFileName = Read-Host "Please enter the original rom file name inside the Input folder"
+    Write-Host "No argument was provided for the original rom file path."
+    $originalRomFilePath = "";
 } else {
-    Write-Host Original Rom File name argument provided: $originalRomFileName
+    Write-Host Original Rom File name argument provided: $originalRomFilePath
 }
 
-$pathTest = Test-Path -Path Input\$originalRomFileName -PathType Leaf;
-$fileExtensionTest = [IO.Path]::GetExtension($originalRomFileName);
+#Initial check to see if GSI path argument was provided or not.
+if([string]::IsNullOrEmpty($GSIFilePath)) {
+    Write-Host "No argument was provided for the GSI File path."
+    $GSIFilePath = "";
+} else {
+    Write-Host GSI File path argument provided: $GSIFilePath;
+}
+Write-Host;
 
-while((-Not $pathTest) -or (-Not ($fileExtensionTest -eq '.img')) -or ([string]::IsNullOrEmpty($originalRomFileName))) {
-    if([string]::IsNullOrEmpty($originalRomFileName)) {
-        Write-Host File name is entereed was null or empty.;
-    } elseif(-Not $pathTest ) {
-        Write-Host $originalRomFileName is not a valid path.;
-    } elseif(-Not ($fileExtensionTest -eq '.img')) {
-        Write-Host $originalRomFileName is not an image file.;
+#Data verification so user can't easily pass in an invalid original rom path.
+if (-not [string]::IsNullOrEmpty($originalRomFilePath)) {
+   $romPathTest = Test-Path -Path $originalRomFilePath -PathType Leaf;
+   $romFileExtensionTest = [IO.Path]::GetExtension($originalRomFilePath);
+}
+
+#Request path from user, looping until a completely valid original rom path is provided.
+while((-Not $romPathTest) -or (-Not ($romFileExtensionTest -eq '.img')) -or ([string]::IsNullOrEmpty($originalRomFilePath))) {
+    $originalRomFilePath = Read-Host -Prompt "Please enter the original rom file path";
+    $originalRomFilePath = $originalRomFilePath.Trim('"');
+    if (-not [string]::IsNullOrEmpty($originalRomFilePath)) {
+        $romPathTest = Test-Path -Path $originalRomFilePath -PathType Leaf;
+        $romFileExtensionTest = [IO.Path]::GetExtension($originalRomFilePath);
+    }
+    if([string]::IsNullOrEmpty($originalRomFilePath)) {
+        Write-Host Original Rom File path entered was null or empty.;
+    } elseif(-Not $romPathTest ) {
+        Write-Host $originalRomFilePath is not a valid path.;
+    } elseif(-Not ($romFileExtensionTest -eq '.img')) {
+        Write-Host $originalRomFilePath does not point to an image file.;
     } 
     Write-Host;
-    $originalRomFileName = Read-Host Please enter the original rom file name inside the Input folder;
-    $pathTest = Test-Path -Path Input\$originalRomFileName -PathType Leaf;
-    $fileExtensionTest = [IO.Path]::GetExtension($originalRomFileName);
-    
 }
 
-Write-Output $originalRomFileName;
+#Data verification so user can't easily pass in an invalid GSI path.
+if (-not [string]::IsNullOrEmpty($GSIFilePath)) {
+    $gsiPathTest = Test-Path -Path $GSIFilePath -PathType Leaf;
+    $gsiFileExtensionTest = [IO.Path]::GetExtension($GSIFilePath);
+}
+
+#Request path from user, looping until a completely valid GSI path is provided.
+while((-Not $gsiPathTest) -or (-Not ($gsiFileExtensionTest -eq '.img')) -or ([string]::IsNullOrEmpty($GSIFilePath))) {
+    $GSIFilePath = Read-Host -Prompt "Please enter the GSI File path";
+    $GSIFilePath = $GSIFilePath.Trim('"');
+    if (-not [string]::IsNullOrEmpty($GSIFilePath)) {
+        $gsiPathTest = Test-Path -Path $GSIFilePath -PathType Leaf;
+        $gsiFileExtensionTest = [IO.Path]::GetExtension($GSIFilePath);
+    }
+    if([string]::IsNullOrEmpty($GSIFilePath)) {
+        Write-Host GSI File path entered was null or empty.;
+    } elseif(-Not $gsiPathTest ) {
+        Write-Host $GSIFilePath is not a valid path.;
+    } elseif(-Not ($gsiFileExtensionTest -eq '.img')) {
+        Write-Host $GSIFilePath does not point to an image file.;
+    } 
+    Write-Host;
+}
+
+
+
 Read-Host -Prompt "Press Enter to exit"
